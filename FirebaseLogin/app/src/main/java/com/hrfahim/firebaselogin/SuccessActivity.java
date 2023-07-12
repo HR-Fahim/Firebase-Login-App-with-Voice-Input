8bcbd4584e785bc060b62469bc9e183719a750e5
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2023-07-12 06:47:52 +0600

    Update Files

---
+++ FirebaseLogin/app/src/main/java/com/hrfahim/firebaselogin/SuccessActivity.java
@@ -0,0 +1,14 @@
+package com.hrfahim.firebaselogin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle SaveInstanceState) {
+        super.onCreate(SaveInstanceState);
+        setContentView(R.layout.activity_success);
+    }
+}
